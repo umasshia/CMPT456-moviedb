b5768255c2eb4008555715bd2534293182dbf3aa
umasshia <89848352+umasshia@users.noreply.github.com>
2022-11-14 23:28:09 -0500

    Update Watchlist.jsx

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -5,9 +5,7 @@ const Watchlist = () => {
   return (
     <div>
       <div className='w-full text-white'>
-        <div className='w-full h-[400px] object-cover'></div>
-        <div className='bg-black/60 fixed top-0 left-0 w-full h-[550px]'>
-        </div>
+        <div className='w-full h-[200px] object-cover'></div>
           <h1 className='absolute right-0 left-0 bottom-0 top-[20%] text-3xl md:text-5xl font-bold text-center'>Watchlist</h1>
       </div>
       <SavedMovies />
