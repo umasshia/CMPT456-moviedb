89a25d1cc7fb66ea66cfb0077fb2e627800ef863
umasshia <giorgisamu.gs@gmail.com>
2022-11-18 14:31:21 -0500

    searchbar changed back

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Search = ({ handleInput }) => {
 
 	return (
-		<div className="absolute w-1/2 text-gray-700 text-xl font-light left-1/4 top-5">
+		<div className=" w-1/2 text-gray-700 m-auto text-xl font-light ">
 			<input 
 				type="text" 
 				placeholder="Search for a movie..." 
-				className="block w-full m-auto border text-[#FFFDE3] border-gray-300 py-2 px-6 bg-transparent" 
+				className="block w-full border text-[#FFFDE3] border-gray-300 py-2 px-6 bg-transparent" 
 				onChange={handleInput}
 			/>
 		</div>
