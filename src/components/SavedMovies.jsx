6f2f0d2eb6c398e74f4692decf4b437e21bccc1d
umasshia <giorgisamu.gs@gmail.com>
2022-11-19 00:15:23 -0500

    ...

--- src/components/SavedMovies.jsx
+++ src/components/SavedMovies.jsx
@@ -3,7 +3,7 @@ import { UserAuth } from "../context/AuthContext";
 import { db } from "../Firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
-import { AiOutlineClose } from "react-icons/ai";
+import { HiTrash } from "react-icons/hi";
 
 const SavedMovies = () => {
     const [movies,setMovies] = useState([])
@@ -17,7 +17,7 @@ const SavedMovies = () => {
     },[user?.email])
 
     const movieRef = doc(db,'users',`${user?.email}`)
-    const deleteMovie = async(passId)=>{
+    const deleteMovie = async(passId)=> {
       try{
         const result = movies.filter((item)=> item.id !== passId)
         await updateDoc(movieRef,{
@@ -31,16 +31,22 @@ const SavedMovies = () => {
   return (
     <div>
       <div className= "mx-auto max-w-2xl py-10 px-2 sm:py-10 sm:px-6 lg:max-w-7xl">
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
           {movies.map((item) => (
-            <div key={item.id} className="inline-block cursor-pointer relative" onClick={() => navigate(`/${item.genre}/${item.id}`)}>
+            <div key={item.id} className="inline-block cursor-pointer relative border-4 border-double border-[#FFFDE3] p-2 z-10" onClick={() => navigate(`/${item.id}`)}>
               <img
                 className="w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500${item?.img}`}
                 alt=""
               />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/60 opacity-0 hover:opacity-100 text-[#FFFDE3]">
-                <p onClick={()=>deleteMovie(item.id)} className="absolute text-[#FFFDE3] top-4 right-4"><AiOutlineClose/></p>
+              <div  className="absolute top-0 left-0 w-full h-full hover:bg-black/60 opacity-0 hover:opacity-100 text-[#FFFDE3] z-20">
+                <div onClick={(e) => {
+                  e.stopPropagation();
+                  deleteMovie(item.id)
+                  }} 
+                  className="absolute text-[#FFFDE3] text-xl top-4 right-4 z-20">
+                  <HiTrash />
+                </div>
               </div>
             </div>
           ))}
